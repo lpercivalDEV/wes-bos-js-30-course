c96372b69f8d3786e5e64387cc41e6907e2dae5b
lpercivalDEV <lpercival.dev@gmail.com>
2018-05-05 23:22:38 -0400

    first commit - setup

---
+++ 01 - JavaScript Drum Kit/lp-code.js
@@ -0,0 +1,6 @@
+//========== Wes Bos - JS 30 Course - My Drumkit Code ==========//
+// ES6 style vanilla javascript! woot!
+
+
+/* If you're intrested in supporting Wes Bos or taking one of his
+awesome courses, click here:  www.courses.wesbos.com   */
