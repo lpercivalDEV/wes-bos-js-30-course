703b594b6aa9b3970c50e04626f446bd63fe2e9e
lpercivalDEV <lpercival.dev@gmail.com>
2018-05-09 21:40:38 -0400

    added array cardio code - pt 1

---
+++ 04 - Array Cardio Day 1/array-cardio-1.js
@@ -0,0 +1,44 @@
+//========== Wes Bos - JS 30 Course - Array Cardio Day 1 ==========//
+// ES6 style vanilla javascript! woot!
+
+/* If you're intrested in supporting Wes Bos or taking one of his
+awesome courses, click here:  www.courses.wesbos.com   */
+
+//=========== ACTUAL CODE ===========//
+
+// let's create an object with some data we can work with:
+
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+];
+
+
+// Here we have an array with people whose last names start with the letter B:
+const people = [ 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William' ];
+
+// Array.prototype.filter()
+// 1. Filter the list of inventors for the ones born in the 1500's:
+
+// the way this filter works, is you pass it a function, and that function loops over all items
+// ... in the array and will pop out the inventor(s) we are looking for:
+const fifteen = inventors.filter(function(inventor){
+    if(inventor.year >= 1500 && inventor.year < 1600){
+        return true; // means we keep that inventors
+        // no need for an 'else' statement to return the "false" results because
+        // ...they are automatically filtered out
+    }
+});
+
+//now lets console.log our variable fifteen to check if it works:
+console.log(fifteen);
